92875099db65c8066a646d03b24d383630db920f
jess-ee <136972746+jess-ee@users.noreply.github.com>
2023-07-05 21:53:54 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 #import dependencies
 import os 
+from apikey import apikey
 import langchain
 import streamlit as st 
 import time
+import pandas as pd
 
 from langchain.llms import OpenAI
 
@@ -14,7 +16,7 @@ from langchain.prompts.chat import (
 )
 
 from langchain.chains import LLMChain
-apikey = os.getenv('OPENAI_API_KEY')
+os.environ['OPENAI_API_KEY'] = apikey
 
 #App framework
 
@@ -22,7 +24,7 @@ st.title ('The NPS label tool 😄🥲😊')
 st.markdown("""
 Introducing our AI-powered feedback analysis tool, designed to unlock customer insights at scale. Utilizing the power of OpenAI's GPT-3, our tool automates the process of deciphering customer sentiment and identifying key themes from your feedback data. It's built to save time, enhance understanding, and empower businesses to act on customer insights faster than ever before. With our tool, make every piece of feedback count!
 """)
-NPSanswer = st.text_input(' **Enter your NPS open answer** ')
+#NPSanswer = st.text_input(' **Enter your NPS open answer** ')
 
 #Chatmodel
 
@@ -31,13 +33,26 @@ chat_model= ChatOpenAI(temperature=0.9, model="gpt-3.5-turbo")
 #Prompt template
 
 system_message_prompt = SystemMessagePromptTemplate.from_template("Pretend you're a customer journey specialist who is an expert in labeling customer feedback.")
-human_message_prompt = HumanMessagePromptTemplate.from_template("**Context**For the consumer electronics store of Coolblue, we receive feedback on how an appointment in one of our stores has gone. **Assignment** In the following assignment, you need to derive from the given text the reasons why they are not satisfied and process this into labels. Use a maximum of 3 labels per answer. If fewer labels are needed, use fewer. Focus the labeling on the cause of the dissatisfaction. **Format**Make sure the labels are as short and concrete as possible. Separate the labels with commas. Use only English language to create labels. **Example** Label, Label, Label' **Input** Label the following NPS input {NPSanswer} ")
+human_message_prompt = HumanMessagePromptTemplate.from_template(" ##Context For the consumer electronics store of Coolblue, we receive feedback on how an appointment in one of our stores has gone. ##Assignment In the following assignment, you need to derive from the given text the reasons why they are not satisfied and process this into labels. Use a maximum of 5 labels for all combined answers. Focus the labeling on the cause of the dissatisfaction. Also provide an explanation of the label, keep general and do not use specific examples. Also provide a weight to each label according to how much it is mentioned in the answers. Use 100 points to divide over the 5 labels.   ##FormatMake sure the labels are as short and concrete as possible.  Use only English language to create labels. ##Input Label the following NPS input {NPSanswer}")
 chat_prompt = ChatPromptTemplate.from_messages([system_message_prompt, human_message_prompt])
 
  #LLM CHain
 
 nps_chain = LLMChain(llm=chat_model, prompt=chat_prompt, verbose = True)
 
+# This line will allow the user to upload an Excel file
+uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
+
+# This line will convert the uploaded file into a pandas DataFrame
+if uploaded_file is not None:
+    df = pd.read_excel(uploaded_file)
+    st.write(df)  # Optional: This will display the DataFrame in the app
+
+# Combine all feedback stories into a single string
+    feedback_text = '\n'.join(df['Feedback'].astype(str))
+    NPSanswer = feedback_text
+    #NPSanswer = st.text_input(' **Enter your NPS open answer** ', value=feedback_text)
+    
 # Show stuff on the screen when there is a prompt 
 if st.button('Generate'):
     try:
@@ -45,13 +60,38 @@ if st.button('Generate'):
             response = nps_chain.run({"NPSanswer": NPSanswer})
             st.write(response)
     except Exception as e:
-        st.error(f"an error occurred:{e}")
+        st.error(f"An error occurred: {e}")
+
+    # This line will create a new column 'insights' in the DataFrame
+    # It will apply the OpenAI model to the combined feedback text
+    #if NPSanswer:
+        #df['insights'] = nps_chain.run({"NPSanswer": NPSanswer})
+        #top_insights = df.sort_values('insights').head(8)
+        #st.write(top_insights)
 
 
 
 
 
 
+
+
+
+
+
+# This line will create a new column 'insights' in the DataFrame
+# It will apply the OpenAI model to each row of the 'feedback' column
+# Replace 'feedback' with the name of the column that contains the feedback
+##if uploaded_file is not None:
+##    df['insights'] = df['feedback'].apply(lambda x: nps_chain.run({"NPSanswer": x}))
+
+# This line will sort the DataFrame by the 'insights' column and return the top 8 rows
+# You may need to modify this line to fit your use case
+# For example, if 'insights' contains numeric values, you might want to sort in descending order
+# If 'insights' contains text, you might need a different method to determine the "top" insights
+##if uploaded_file is not None:
+##    top_insights = df.sort_values('insights').head(8)
+##    st.write(top_insights)
 
 
 #uploaded_file = st.file_uploader("Choose a file")
